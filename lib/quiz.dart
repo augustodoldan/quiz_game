e4943af82a0ff592603206b9b9c9348a4905d6de
Augusto Javier Doldán <augusto.doldan@hotmail.com>
2023-05-18 19:05:35 -0300

    I have removed the erroneous validation

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -26,8 +26,6 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        selectedAnswers = [];
-
         activeScreen = "results-widget";
       });
     }
